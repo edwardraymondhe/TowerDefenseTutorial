2950037bb937b440f0fac460b05c9a2d3c18b019
edwardraymondhe <edward.raymond.he@hotmail.com>
2020-07-12 20:27:22 +0800

    feat: added turning mechanics for the turrent

---
+++ Assets/Scripts/Turrent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Turrent : MonoBehaviour
+{
+    public Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float rotateSpeed = 10f;
+
+    private void Start()
+    {
+        // Invoke repeating function
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        // Get the array of the enemies
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if(nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;    // Transform is the gameObject
+            // The head is responsible for shooting...?
+
+        }else if(nearestEnemy == null)
+        {
+            nearestEnemy = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (target == null)
+            return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * rotateSpeed).eulerAngles;
+
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected() // Drawn when selected
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
